68d6a2832ff36920ffdb44725961586a07b3ae41
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2022-04-21 15:58:35 -0300

    Add project to repository

---
+++ BinaryGap/BinaryGap/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BinaryGap {
+	class MainClass {
+		public static void Main(string[] args) {
+			var _solution = new Solution();
+			Console.WriteLine(_solution.solution(1041));
+		}
+	}
+}
---
+++ BinaryGap/BinaryGap/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryGap
+{
+	public class Solution {
+		public int solution(int N) {
+			int value = 0;
+			var results = new List<int>();
+			var binary = Convert.ToString(N, 2);
+
+			foreach (var c in binary) {
+				if(c.Equals('0')) {
+					value += 1;
+				} else {
+					results.Add(value);
+					value = 0;
+				}
+			}
+
+			return results.Max();
+		}
+	}
+}
